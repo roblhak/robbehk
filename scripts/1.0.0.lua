a78f1f2949a14cbf9309d2800e43183af7da7710
roblhak <107290832+roblhak@users.noreply.github.com>
2022-06-11 16:47:16 +0900

    Update 1.0.0.lua

--- scripts/1.0.0.lua
+++ scripts/1.0.0.lua
@@ -1,2 +1,21 @@
-local players=game:GetService("Players")
-local locPlayer=PLAYERS.LocalPlayer
+print("made by roblhak, version: 1.0.0")
+
+local ScreenGui = Instance.new("ScreenGui")
+local EndGame = Instance.new("TextLabel")
+
+ScreenGui.Parent = game.CoreGui;
+
+EndGame.Name = "EndGame"
+EndGame.Parent = ScreenGui
+EndGame.BackgroundColor3 = Color3.fromRGB(0, 0, 50)
+EndGame.BorderColor3 = Color3.fromRGB(0, 0, 50)
+EndGame.BorderSizePixel = 0
+EndGame.Position = UDim2.new(0.0234833658, 0, 0.0377952754, 0)
+EndGame.Size = UDim2.new(0, 122, 0, 28)
+EndGame.Font = Enum.Font.SourceSans
+EndGame.Text = "EndGame"
+EndGame.TextColor3 = Color3.fromRGB(255, 255, 255)
+EndGame.TextSize = 14.000
+EndGame.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+EndGame.Active=true
+EndGame.Draggable=true
